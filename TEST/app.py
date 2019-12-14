8e9eeb8ab073a6962d5652cbce16b83e85e64cbb
JimKing100 <52686228+JimKing100@users.noreply.github.com>
2019-12-13 16:44:53 -0800

    Add files via upload

--- TEST/app.py
+++ TEST/app.py
@@ -4,6 +4,7 @@ from decouple import config
 from flask import Flask, render_template
 from TEST.models import DB, User
 import swapi
+import requests
 
 
 def create_app():
@@ -19,15 +20,20 @@ def create_app():
     def home():
         DB.drop_all()
         DB.create_all()
-        person = swapi.get_person(1)
-        person1 = person.name
-        person = swapi.get_person(2)
-        person2 = person.name
+
+        response = requests.get("https://swapi.co/api/people/?search=l")
+        data = response.json()
+        person = data['results'][0]
+        # person = swapi.get_person(1)
+        person1 = person['name']
+        person = data['results'][1]
+        # person = swapi.get_person(2)
+        person2 = person['name']
         db_user = User(id='1', name=person1)
         DB.session.add(db_user)
         db_user = User(id='2', name=person2)
         DB.session.add(db_user)
         DB.session.commit()
-        people = User.query.filter(User.name == person1)
+        people = User.query.filter(User.name == person2)
         return render_template('home.html', title=person1, people=people)
     return app
